4e7b51ce9074c8ac7a9a09ae055f25b3401e0bc3
Jeff Malnick <malnick@gmail.com>
2020-10-30 08:35:55 -0700

    fix layout

--- deployment/aws/vars.tf
+++ deployment/aws/vars.tf
@@ -1,60 +1,7 @@
-resource "random_pet" "test" {
-  length = 1
-}
-
-locals {
-  tags = {
-    Name = "${var.tag}-${random_pet.test.id}"
-  }
-
-  pub_cidrs  = cidrsubnets("10.0.0.0/24", 4, 4, 4, 4)
-  priv_cidrs = cidrsubnets("10.0.100.0/24", 4, 4, 4, 4)
-}
-
-variable "tag" {
-  default = "boundary-test"
-}
-
 variable "boundary_bin" {
   default = "~/projects/boundary/bin"
 }
 
 variable "pub_ssh_key_path" {
   default = "~/.ssh/id_rsa.pub"
-}
-
-variable "num_workers" {
-  default = 1
-}
-
-variable "num_controllers" {
-  default = 2
-}
-
-variable "num_targets" {
-  default = 1
-}
-
-variable "num_subnets_public" {
-  default = 2
-}
-
-variable "num_subnets_private" {
-  default = 2
-}
-
-variable "tls_cert_path" {
-  default = "/etc/pki/tls/boundary/boundary.cert"
-}
-
-variable "tls_key_path" {
-  default = "/etc/pki/tls/boundary/boundary.key"
-}
-
-variable "tls_disabled" {
-  default = true
-}
-
-variable "kms_type" {
-  default = "aws"
 }
