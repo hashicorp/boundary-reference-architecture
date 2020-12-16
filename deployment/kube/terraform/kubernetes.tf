6b0d8573dec90f5782252aab492c6d208a326da5
Jeff Malnick <malnick@gmail.com>
2020-12-16 11:31:19 -0800

    use init on boundary

--- deployment/kube/terraform/kubernetes.tf
+++ deployment/kube/terraform/kubernetes.tf
@@ -18,8 +18,6 @@ resource "kubernetes_deployment" "postgres" {
   }
 
   spec {
-
-
     replicas = 1
 
     selector {
@@ -40,30 +38,9 @@ resource "kubernetes_deployment" "postgres" {
           name = "boundary-config"
         }
 
-        init_container {
-          name    = "postgres-init"
-          image   = "hashicorp/boundary:0.1.2"
-          command = ["database", "init", "-config", "/boundary/boundary.hcl"]
-          volume_mount {
-            name       = "boundary-config"
-            mount_path = "/boundary"
-            read_only  = true
-          }
-
-          env {
-            name  = "BOUNDARY_PG_URL"
-            value = "postgresql://postgres:postgres@127.0.0.1/boundary?sslmode=disable"
-          }
-        }
-
         container {
           image = "postgres"
           name  = "postgres"
-          volume_mount {
-            name       = "boundary-config"
-            mount_path = "/boundary"
-            read_only  = true
-          }
 
           env {
             name  = "POSTGRES_DB"
@@ -89,8 +66,6 @@ resource "kubernetes_deployment" "postgres" {
               command = ["psql", "-w", "-U", "postgres", "-d", "boundary", "-c", "SELECT", "1"]
             }
           }
-
-
         }
       }
     }
@@ -110,6 +85,81 @@ resource "kubernetes_service" "postgres" {
     port {
       port     = 5432
       protocol = "TCP"
+    }
+  }
+}
+
+resource "kubernetes_deployment" "boundary" {
+  metadata {
+    name = "boundary"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        run = "boundary"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          run = "boundary"
+        }
+      }
+
+      spec {
+        volume {
+          name = "boundary-config"
+        }
+
+        init_container {
+          name    = "database-init"
+          image   = "hashicorp/boundary:0.1.2"
+          command = ["database", "init", "-config", "/boundary/boundary.hcl"]
+          volume_mount {
+            name       = "boundary-config"
+            mount_path = "/boundary"
+            read_only  = true
+          }
+
+          env {
+            name  = "BOUNDARY_PG_URL"
+            value = "postgresql://postgres:postgres@127.0.0.1/boundary?sslmode=disable"
+          }
+        }
+
+        container {
+          image = "hashicorp/boundary:0.1.2"
+          name  = "boundary"
+          volume_mount {
+            name       = "boundary-config"
+            mount_path = "/boundary"
+            read_only  = true
+          }
+
+          env {
+            name  = "POSTGRES_DB"
+            value = "boundary"
+          }
+
+          env {
+            name  = "POSTGRES_USER"
+            value = "postgres"
+          }
+
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = "postgres"
+          }
+
+          port {
+            container_port = 9200
+          }
+        }
+      }
     }
   }
 }
