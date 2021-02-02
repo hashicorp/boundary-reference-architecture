8f25a20bf8d65873de409675b8494bf8fdb8df64
Jeff Malnick <malnick@gmail.com>
2021-02-02 10:41:44 -0800

    refactor for 1.5

--- deployment/kube/kubernetes/boundary.tf
+++ deployment/kube/kubernetes/boundary.tf
@@ -34,7 +34,7 @@ resource "kubernetes_deployment" "boundary" {
 
         init_container {
           name    = "boundary-init"
-          image   = "hashicorp/boundary:0.1.4"
+          image   = "hashicorp/boundary:0.1.5"
           command = ["/bin/sh", "-c"]
           args    = ["boundary database init -config /boundary/boundary.hcl"]
 
