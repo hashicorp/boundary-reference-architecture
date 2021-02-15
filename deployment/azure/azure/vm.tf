74a753358ac5768f0e359e36ffa9e4662c8aa8e7
ned1313 <ned.bellavance@gmail.com>
2021-02-15 10:42:17 -0500

    remove template file data source

--- deployment/azure/azure/vm.tf
+++ deployment/azure/azure/vm.tf
@@ -61,24 +61,6 @@ resource "azurerm_network_interface_application_security_group_association" "con
   application_security_group_id = azurerm_application_security_group.controller_asg.id
 }
 
-data "template_file" "controller" {
-  template = file("${path.module}/boundary.tmpl")
-
-  vars = {
-    vault_name       = local.vault_name
-    type             = "controller"
-    name             = "boundary"
-    boundary_version = var.boundary_version
-    tenant_id        = data.azurerm_client_config.current.tenant_id
-    db_username      = var.db_username
-    db_password      = var.db_password
-    db_endpoint      = azurerm_postgresql_server.boundary.fqdn
-    db_name          = local.pg_name
-    public_ip        = azurerm_public_ip.boundary.ip_address
-    controller_ips   = join(",", azurerm_network_interface.controller[*].private_ip_address)
-  }
-}
-
 resource "azurerm_linux_virtual_machine" "controller" {
   count               = var.controller_vm_count
   name                = "${local.controller_vm}-${count.index}"
@@ -169,24 +151,6 @@ resource "azurerm_network_interface_application_security_group_association" "wor
   count                         = var.worker_vm_count
   network_interface_id          = azurerm_network_interface.worker[count.index].id
   application_security_group_id = azurerm_application_security_group.worker_asg.id
-}
-
-data "template_file" "worker" {
-  template = file("${path.module}/boundary.tmpl")
-
-  vars = {
-    vault_name       = local.vault_name
-    type             = "worker"
-    name             = "boundary"
-    boundary_version = var.boundary_version
-    tenant_id        = data.azurerm_client_config.current.tenant_id
-    public_ip        = azurerm_public_ip.boundary.ip_address
-    controller_ips   = join(",", azurerm_network_interface.controller[*].private_ip_address)
-    db_username      = var.db_username
-    db_password      = var.db_password
-    db_name          = local.pg_name
-    db_endpoint      = azurerm_postgresql_server.boundary.fqdn
-  }
 }
 
 resource "azurerm_linux_virtual_machine" "worker" {
