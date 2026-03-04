b623ca4402fb7c66bc64ac5141f6b2caf78e5080
Andre Faria <andre.faria.24@gmail.com>
2026-03-04 11:15:40 -0500

    replaced region hardcoding with variable

--- deployment/aws/aws/net.tf
+++ deployment/aws/aws/net.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = var.aws_region
 }
 
 data "aws_availability_zones" "available" {
--- deployment/aws/aws/vars.tf
+++ deployment/aws/aws/vars.tf
@@ -65,3 +65,7 @@ variable "tls_disabled" {
 variable "kms_type" {
   default = "aws"
 }
+
+variable "aws_region" {
+  default = "us-west-2"
+}
